69afaddb09f00438d235105c881ce7ac5b1bc00d
Thiago Melo <thiago_degan@gsretail.com.br>
2021-06-16 22:20:06 -0300

    Config Prod

--- setup/settings.py
+++ setup/settings.py
@@ -83,6 +83,18 @@ if 'WEBSITE_HOSTNAME' not in os.environ:
         }
     }
 
+hostname = os.environ['DBHOST']
+if 'WEBSITE_HOSTNAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ['DBNAME'],
+            'HOST': hostname + ".postgres.database.azure.com",
+            'USER': os.environ['DBUSER'] + "@" + hostname,
+            'PASSWORD': os.environ['DBPASS'] 
+        }
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
