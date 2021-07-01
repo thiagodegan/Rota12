5b9c52bd3e435bb0c2c38b08c62cbf9afdec747d
Thiago Melo <thiago_degan@gsretail.com.br>
2021-06-30 21:49:37 -0300

    #6 - Tela index prototipo

--- rota12/static/js/layout.js
+++ rota12/static/js/layout.js
@@ -1,11 +1,16 @@
 $(document).ready(function () {
-    /*$('#appBar').kendoAppBar({
+    $('#appBar').kendoAppBar({
         positionMode: "sticky",
         position: "top",
         items: [
-            { template: '<h1>Rota 12</h1>', type: "contentItem" },
+            { template: '<h1 class="links">Rota 12</h1>', type: "contentItem" },
             { type: "spacer" },
-            { template: '<button>Registrar</button>', type: "contentItem"}
+            { template: '<button id="BtnRegistrar">Registrar</button>', type: "contentItem"},
+            { type: "spacer", width: 1},
+            { template: '<button id="BtnEntrar" class="k-primary">Entrar</button>', type: "contentItem"},
         ]
-    });*/
+    });
+
+    $('#BtnRegistrar').kendoButton();
+    $('#BtnEntrar').kendoButton();
 });
--- rota12/static/kendo-ui/js/cultures/kendo.culture.ta-IN.min.js
+++ rota12/static/kendo-ui/js/cultures/kendo.culture.ta-IN.min.js
@@ -1,6 +1,6 @@
 /** 
- * Kendo UI v2018.3.1017 (http://www.telerik.com/kendo-ui)                                                                                                                                              
- * Copyright 2018 Telerik EAD. All rights reserved.                                                                                                                                                     
+ * Kendo UI v2021.2.616 (http://www.telerik.com/kendo-ui)                                                                                                                                               
+ * Copyright 2021 Progress Software Corporation and/or one of its subsidiaries or affiliates. All rights reserved.                                                                                      
  *                                                                                                                                                                                                      
  * Kendo UI commercial licenses may be obtained at                                                                                                                                                      
  * http://www.telerik.com/purchase/license-agreement/kendo-ui-complete                                                                                                                                  
