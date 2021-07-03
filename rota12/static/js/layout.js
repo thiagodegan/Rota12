c2f4ebd12ceae2fe79f0317025a9be7032b83d31
Thiago Melo <thiago_degan@gsretail.com.br>
2021-07-02 22:01:29 -0300

    removido telerik

--- rota12/static/js/layout.js
+++ rota12/static/js/layout.js
@@ -1,16 +1,3 @@
 $(document).ready(function () {
-    $('#appBar').kendoAppBar({
-        positionMode: "sticky",
-        position: "top",
-        items: [
-            { template: '<h1 class="links">Rota 12</h1>', type: "contentItem" },
-            { type: "spacer" },
-            { template: '<button id="BtnRegistrar">Registrar</button>', type: "contentItem"},
-            { type: "spacer", width: 1},
-            { template: '<button id="BtnEntrar" class="k-primary">Entrar</button>', type: "contentItem"},
-        ]
-    });
-
-    $('#BtnRegistrar').kendoButton();
-    $('#BtnEntrar').kendoButton();
+    
 });
--- rota12/views.py
+++ rota12/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def index(request):
@@ -7,3 +8,11 @@ def index(request):
 
 def login(request):
     return HttpResponse('Pagina de login')
+
+def registro(request):
+    if request.method == 'GET':
+        return render(request, 'home/registro.html')
+    elif request.method == 'POST':
+        username = request.POST['Login']
+        password = request.POST['Password']
+        return redirect('index')
